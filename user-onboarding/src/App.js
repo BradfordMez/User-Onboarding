d7bd897369e466dbc91c5d79ab62be7f9fa7cf52
Bradford-Meziere <coleokc@gmail.com>
2021-07-22 16:32:59 -0500

    Styled

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,6 +2,18 @@ import './App.css';
 import UserForm from './form';
 import React,{useState} from 'react';
 import IndividualUser from './IndividualUser';
+import styled from 'styled-components'
+
+const WebsiteDiv = styled.div`
+    text-align: center;
+    background-color: ${props=>props.theme.headerColor};
+    color: ${props=>props.theme.primaryColor};
+    padding-top:1em;
+    padding-bottom:1em;
+    margin:.3em;
+    border: .8em ridge ${props=>props.theme.tertiaryColor};
+`
+
 
 function App() {
   const [team, setTeam] = useState([])
@@ -11,12 +23,12 @@ function App() {
 
   return (
 
-    <div className="App">
+    <WebsiteDiv className="App">
       <UserForm addToTeam={addToTeam}/>
       {team.map((member)=>{
         return <IndividualUser IndividualUser={member}/>
       })}
-    </div>
+    </WebsiteDiv>
   );
 }
 
--- user-onboarding/src/IndividualUser.js
+++ user-onboarding/src/IndividualUser.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+
+const StyledH2 = styled.h2`
+    background-color: ${props => props.theme.backgroundColor};
+    margin-left: 18em;
+    margin-right:18em;
+    padding-top:.5em;
+    padding-bottom:.5em;
+    border: .4em double ${props=>props.theme.tertiaryColor};
+    border-radius: 1em;
+    color: ${props => props.theme.primaryColor};
+    font-size: 2em;
+`
+const StyledInfo=styled.p`
+  background-color: ${props=>props.theme.secondaryColor};
+  padding-right:2rem;
+  padding-left:2rem;
+  padding-top:1rem;
+  padding-bottom:1rem;
+  margin-left:2rem;
+  margin-right:2rem;
+  border: 1em double ${props=>props.theme.tertiaryColor};
+  border-radius:1em;
+  font-size:.6em;
+`
+
 
 export default function IndividualUser(props) {
     return (
         <div>
-            <h2>{props.IndividualUser.name}</h2>
-            <p>{props.IndividualUser.email}</p>
-            <p>{props.IndividualUser.password}</p>
+            <StyledH2>{props.IndividualUser.name}
+            <StyledInfo>Email: {props.IndividualUser.email}</StyledInfo>
+            <StyledInfo>Password: {props.IndividualUser.password}</StyledInfo></StyledH2>
         </div>
     )
 }
